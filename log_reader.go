90e0cba3f643de25265423d31798dec23004cb40
Sunjin Lee <styner32@gmail.com>
2014-02-02 15:11:22 +0800

    change package name re-indent code change parsing header function name to 'ParseHeader'

--- log_reader.go
+++ log_reader.go
@@ -1,4 +1,4 @@
-package main
+package log_reader
 
 import "os"
 import "log"
@@ -9,35 +9,35 @@ import "encoding/json"
 import "strconv"
 
 type Header struct {
-	Month string
-	Date string
-	Time string
-	Ip string
-	Uuid string
+	Month    string
+	Date     string
+	Time     string
+	Ip       string
+	Uuid     string
 	Username string
 }
 
 type Body struct {
 	RequestStart *RequestStartBody
-	Processor *ProcessorBody
-	Parameters *ParametersBody
-	Complete *CompleteBody
-	Length int
-	Contents []string
+	Processor    *ProcessorBody
+	Parameters   *ParametersBody
+	Complete     *CompleteBody
+	Length       int
+	Contents     []string
 }
 
 type RequestStartBody struct {
 	Action string
-	Url string
-	Ip string
-	Date string
-	Time string
+	Url    string
+	Ip     string
+	Date   string
+	Time   string
 }
 
 type ProcessorBody struct {
 	ControllerName string
-	Action string
-	MimeType string
+	Action         string
+	MimeType       string
 }
 
 type ParametersBody struct {
@@ -45,9 +45,9 @@ type ParametersBody struct {
 }
 
 type CompleteBody struct {
-	StatusCode string
-	TotalTime float64
-	DatabaseTime float64
+	StatusCode     string
+	TotalTime      float64
+	DatabaseTime   float64
 	ViewRenderTime float64
 }
 
@@ -74,117 +74,117 @@ func main() {
 	}
 
 	for i := 0; i < log_count; i++ {
-		header, body := ParseLog(logs[i])
+		header, body := ParseHeader(logs[i])
 
 		if header == nil {
 			fmt.Printf("failed to parse header : %s\n", logs[i])
 			continue
 		}
 
-		if value,ok := headers[header.Uuid]; ok {
+		if value, ok := headers[header.Uuid]; ok {
 			bodies[value.Uuid].AddContent(body)
 		} else {
 			headers[header.Uuid] = header
-			bodies[header.Uuid] = &Body {Length: 0, Contents: make([]string, 32)}
+			bodies[header.Uuid] = &Body{Length: 0, Contents: make([]string, 32)}
 			bodies[header.Uuid].AddContent(body)
 		}
 	}
 
 	resultFile, err := os.Create("result.json")
- 	if err != nil {
+	if err != nil {
 		log.Fatal(err)
- 	}
- 	defer resultFile.Close()
+	}
+	defer resultFile.Close()
 
 	for uuid, header := range headers {
-   		header_in_json, err := json.Marshal(header)
-     	if err != nil {
-			log.Fatal(err)
-     	}
-
-     	headerLength, err := resultFile.Write(header_in_json)
+		header_in_json, err := json.Marshal(header)
 		if err != nil {
 			log.Fatal(err)
-     	}
-     	resultFile.WriteString("\n")
-     	fmt.Println(headerLength)
+		}
 
-    	body_in_json, err := json.Marshal(bodies[uuid])
-     	if err != nil {
+		headerLength, err := resultFile.Write(header_in_json)
+		if err != nil {
 			log.Fatal(err)
-     	}
+		}
+		resultFile.WriteString("\n")
+		fmt.Println(headerLength)
+
+		body_in_json, err := json.Marshal(bodies[uuid])
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		bodyLength, err := resultFile.Write(body_in_json)
 		if err != nil {
 			log.Fatal(err)
-     	}
-     	resultFile.WriteString("\n")
-     	fmt.Println(bodyLength)
-    }
-    resultFile.Sync()
+		}
+		resultFile.WriteString("\n")
+		fmt.Println(bodyLength)
+	}
+	resultFile.Sync()
 
-    fmt.Println("Done!")
+	fmt.Println("Done!")
 }
 
-func ParseLog(str string) (*Header, string) {
+func ParseHeader(str string) (*Header, string) {
 	expr := `(\w{3}) (\d+) (\d{2}:\d{2}:\d{2}) ([\d\.]+) production.log: \[([\w\d-]+)\] (\[([\w\d]+)\])?(.*)`
 	log, _ := regexp.Compile(expr)
 	if len(log.FindString(str)) > 0 {
 		matches := log.FindStringSubmatch(str)
 		header := &Header{
-			Month: matches[1],
-			Date: matches[2],
-			Time: matches[3],
-			Ip: matches[4],
-			Uuid: matches[5],
-			Username: matches[7] }
+			Month:    matches[1],
+			Date:     matches[2],
+			Time:     matches[3],
+			Ip:       matches[4],
+			Uuid:     matches[5],
+			Username: matches[7]}
 		return header, matches[8]
 	}
 	return nil, ""
 }
 
-func ParseRequestStartBody(str string) (*RequestStartBody) {
+func ParseRequestStartBody(str string) *RequestStartBody {
 	expr := `Started (\w+) "([^"]+)" for ([\d\.]+) at (\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2} [+-]\d{4})`
 	log, _ := regexp.Compile(expr)
 	if len(log.FindString(str)) > 0 {
 		matches := log.FindStringSubmatch(str)
 		request_start_body := &RequestStartBody{
 			Action: matches[1],
-			Url: matches[2],
-			Ip: matches[3],
-			Date: matches[4],
-			Time: matches[5] }
+			Url:    matches[2],
+			Ip:     matches[3],
+			Date:   matches[4],
+			Time:   matches[5]}
 		return request_start_body
 	}
 	return nil
 }
 
-func ParseProcessorBody(str string) (*ProcessorBody) {
+func ParseProcessorBody(str string) *ProcessorBody {
 	expr := `Processing by ([:\w]+)#(\w+) as (.*)?`
 	log, _ := regexp.Compile(expr)
 	if len(log.FindString(str)) > 0 {
 		matches := log.FindStringSubmatch(str)
 		processor_body := &ProcessorBody{
 			ControllerName: matches[1],
-			Action: matches[2],
-			MimeType: matches[3] }
+			Action:         matches[2],
+			MimeType:       matches[3]}
 		return processor_body
 	}
 	return nil
 }
 
-func ParseParametersBody(str string) (*ParametersBody) {
+func ParseParametersBody(str string) *ParametersBody {
 	expr := `Parameters: (.*)`
 	log, _ := regexp.Compile(expr)
 	if len(log.FindString(str)) > 0 {
 		matches := log.FindStringSubmatch(str)
-		parameter_body := &ParametersBody{ Parameters: matches[1] }
+		parameter_body := &ParametersBody{Parameters: matches[1]}
 		return parameter_body
 	}
 	return nil
 }
 
-func ParseCompleteBody(str string) (*CompleteBody) {
+func ParseCompleteBody(str string) *CompleteBody {
 	expr := `Completed (\d+) [\w\s]+ in ([\d\.]+)m?s \(Views: ([\d\.]+)m?s \| ActiveRecord: ([\d\.]+)m?s\)`
 	log, _ := regexp.Compile(expr)
 	if len(log.FindString(str)) > 0 {
@@ -193,10 +193,10 @@ func ParseCompleteBody(str string) (*CompleteBody) {
 		database_time, _ := strconv.ParseFloat(matches[3], 64)
 		view_render_time, _ := strconv.ParseFloat(matches[4], 64)
 		complete_body := &CompleteBody{
-			StatusCode: matches[1],
-			TotalTime: total_time,
-			DatabaseTime: database_time,
-			ViewRenderTime: view_render_time }
+			StatusCode:     matches[1],
+			TotalTime:      total_time,
+			DatabaseTime:   database_time,
+			ViewRenderTime: view_render_time}
 		return complete_body
 	}
 	return nil
